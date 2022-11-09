50699a563eb56ef06a54d98b8e5db081f8b744b5
Gaëtan Didderen <gaetan@keiz.ai>
2022-11-09 12:07:53 +0100

    Now fetch data from placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,70 +1,27 @@
-import Image from 'next/image';
 import React from 'react';
 import Card from '../components/Card';
-import Dashboard from '../public/images/dashboard.jpg';
-import Keypoint from '../public/images/keypoint-1.jpg';
-import Security from '../public/images/security.jpg';
 
-export const Page = () => {
-  const cards = [
-    <Card
-      key={1}
-      title="Dépense annuelle à date"
-      content={<h2>1 057 160 €</h2>}
-    />,
-    <Card
-      key={2}
-      title="Dépense annuelle à date"
-      content={
-        <div className="relative max-w-[200px]">
-          <Image src={Security} alt="security" layout={'responsive'} />
-        </div>
-      }
-    />,
-    <Card
-      key={3}
-      title="Dépense annuelle à date"
-      content={<h2>1 057 160 €</h2>}
-    />,
-    <Card
-      key={4}
-      title="Dépense annuelle à date"
-      content={<h2>1 057 160 €</h2>}
-    />,
-    <Card
-      key={5}
-      title="Dépense annuelle à date"
-      content={
-        <div className="relative max-w-[200px]">
-          <Image src={Dashboard} alt="dashboard" layout={'responsive'} />
-        </div>
-      }
-    />,
-    <Card
-      key={6}
-      title="Dépense annuelle à date"
-      content={<h2>1 057 160 €</h2>}
-    />,
-    <Card
-      key={7}
-      title="Dépense annuelle à date"
-      content={<h2>1 057 160 €</h2>}
-    />,
-    <Card
-      key={8}
-      title="Dépense annuelle à date"
-      content={
-        <div className="relative max-w-[200px]">
-          <Image src={Keypoint} alt="keypoint" layout={'responsive'} />
-        </div>
-      }
-    />,
-  ];
+const getComments = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/comments');
+  return res.json();
+};
+
+export interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+export const Page = async () => {
+  const comments: Comment[] = await getComments();
+
   return (
     <div className="bg-gray-100 w-screen h-screen">
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
-        {cards.map((card, index) => (
-          <div key={index}>{card}</div>
+        {comments.map((comment) => (
+          <Card key={comment.id} title={comment.email} content={comment.body} />
         ))}
       </div>
     </div>
